7e96a94e40c3bd79603f7190d8f69fb30ebc9653
varsha rana <ranavarshaaa19@gmail.com>
2025-02-02 20:25:12 +0530

    new updates

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+// Typing effect logic
+const typedTextSpan = document.querySelector(".typing");
+const textArray = ["A DevOps Engineer", "Cloud Enthusiast", "Bigquery Expert"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (textArray.length) setTimeout(type, newTextDelay);
+});
