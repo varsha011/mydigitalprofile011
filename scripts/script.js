ad8996fc7eab065c642ea0f0b5eeca756d1ee055
varsha rana <ranavarshaaa19@gmail.com>
2025-02-02 21:16:25 +0530

    changes111

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
-// Typing effect for the text in the home section
+// Typing effect logic
 const typedTextSpan = document.querySelector(".typing");
-const textArray = ["Your Name", "A Web Developer", "A Tech Enthusiast"];
+const textArray = ["A Cloud & Infrastructure Engineer", "GCP Specialist", "Datadog Expert"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
 
@@ -8,9 +11,9 @@ function type() {
     if (charIndex < textArray[textArrayIndex].length) {
         typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
         charIndex++;
-        setTimeout(type, 150);  // Adjust speed of typing
+        setTimeout(type, typingDelay);
     } else {
-        setTimeout(erase, 1500);  // Start erasing after delay
+        setTimeout(erase, newTextDelay);
     }
 }
 
@@ -18,16 +21,14 @@ function erase() {
     if (charIndex > 0) {
         typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
         charIndex--;
-        setTimeout(erase, 100);  // Adjust speed of erasing
+        setTimeout(erase, erasingDelay);
     } else {
         textArrayIndex++;
         if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-        setTimeout(type, 200);  // Start typing next text
+        setTimeout(type, typingDelay);
     }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    if (textArray.length) {
-        setTimeout(type, 200);  // Starts typing after a short delay
-    }
+    if (textArray.length) setTimeout(type, newTextDelay);
 });
